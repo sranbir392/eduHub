4d796b359a91c7378de888e90ef8da5074156f74
sranbir392 <sranbir392@hotmail.com>
2024-03-12 04:01:04 +0530

    Update connection.js

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,6 +1,10 @@
 const mongoose =require("mongoose");
 require("dotenv").config();
 
-const connection=mongoose.connect(process.env.MONGOSHURL);
+const connection=mongoose.connect(process.env.MONGOSHURL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
 
 module.exports={connection}
